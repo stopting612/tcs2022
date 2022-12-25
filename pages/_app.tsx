1d467ed3965abd8b400d3423337f5b3e48f2fea7
hoiting <htleung@starrystep.com>
2022-12-25 13:10:53 +0800

    message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { Session } from "next-auth";
+import type { AppProps } from "next/app";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import { SessionProvider } from "next-auth/react";
+import { appWithTranslation } from 'next-i18next'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({
+  Component,
+  pageProps
+}: AppProps<{
+  session: Session;
+}>) {
+  return (
+    <>
+      <SessionProvider session={pageProps.session}>
+        <main>
+            <Component {...pageProps} />
+        </main>
+        <Footer />
+      </SessionProvider>
+    </>
+  );
 }
 
-export default MyApp
+
+export default appWithTranslation(MyApp);
